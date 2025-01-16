96b4a91df3893608cf196556ad633088d29dec69
Gaurav Kumar <GAURAVRK215@GMAIL.COM>
2025-01-16 19:14:23 +0530

    new

---
+++ src/main/java/com/navonmesa/test/event_management/Entity/Event_Management.java
@@ -0,0 +1,40 @@
+package com.realnet.event_management.Entity;
+ import lombok.*;
+
+import javax.persistence.*;
+
+import com.realnet.WhoColumn.Entity.Extension;
+
+import java.time.LocalDateTime;
+ import java.util.*;
+
+ @Entity 
+ @Data
+ public class    Event_Management extends Extension { 
+ /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+ @Id
+ @GeneratedValue(strategy = GenerationType.IDENTITY)
+ private Integer id;
+
+private String  practice_match;
+
+private String  admin_name;
+
+private String  ground;
+
+private String datetime;
+
+private String  name;
+
+
+@Column(length = 2000)
+private String description;
+
+private boolean active;
+
+
+}
