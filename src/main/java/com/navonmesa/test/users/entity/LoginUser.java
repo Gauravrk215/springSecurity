b78f79b3e7fe84c2fae62421bb7f0847eb0b560d
Gaurav Kumar <GAURAVRK215@GMAIL.COM>
2025-02-20 10:34:17 +0530

    table

---
+++ src/main/java/com/navonmesa/test/users/entity/LoginUser.java
@@ -0,0 +1,18 @@
+package com.realnet.users.entity;
+
+import lombok.Data;
+
+@Data
+public class LoginUser {
+
+	// private String username;
+	private String email;
+	private String password;
+
+	/*
+	 * public String getUsername() { return username; }
+	 * 
+	 * public void setUsername(String username) { this.username = username; }
+	 */
+
+}
