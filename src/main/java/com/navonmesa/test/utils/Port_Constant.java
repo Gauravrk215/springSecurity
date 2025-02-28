e6f3e5b54a13ead6b72c800be890f5fb38a78489
Gaurav Kumar <GAURAVRK215@GMAIL.COM>
2025-02-28 09:43:20 +0530

    util

---
+++ src/main/java/com/navonmesa/test/utils/Port_Constant.java
@@ -0,0 +1,15 @@
+package com.realnet.utils;
+
+public class Port_Constant {
+
+	public final static String LOCAL_HOST = "43.205.154.152";
+	public final static String FRONTEND_PORT_9191 = "30165";
+	public static String SURE_SETU_DOMAIN = "http://34.198.218.30:30173";
+
+	public final static String GITEA_IP_ADDRESS = "try.gitea";
+	public final static String GITEA_PORT = "io";
+	public final static String SURE_VAULT_DOMAIN = "http://54.92.243.148:30150";
+	public final static String SUREVAULT_DEPLOYMENT_TYPE = "32";
+	public static String FRONTEND_PORTAL_DOMAIN;
+
+}
